1ee94902db38fe656020f2bafbcadf7aa3f1ca3c
kobe <13811421514@sina.cn>
2017-06-05 14:04:23 +0800

    upload a empty project

---
+++ test001/test001/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test001
+//
+//  Created by 马坚辉 on 2017/6/5.
+//  Copyright © 2017年 马坚辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test001/test001/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test001
+//
+//  Created by 马坚辉 on 2017/6/5.
+//  Copyright © 2017年 马坚辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
